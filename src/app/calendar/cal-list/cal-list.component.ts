8f3aa8f329ace95701cd8ef648114899c1d2a89b
mikelurban <mhop@outlook.com>
2020-12-02 18:08:03 +0100

    chg: style of cal-list

--- src/app/calendar/cal-list/cal-list.component.ts
+++ src/app/calendar/cal-list/cal-list.component.ts
@@ -1,8 +1,19 @@
 import { ViewportRuler } from '@angular/cdk/scrolling';
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { CalendarService } from 'src/app/services/calendar.service';
-import { AppEvents } from 'src/app/services/models/events';
+import { AppEvent, AppEvents } from 'src/app/services/models/events';
+
+export interface listEventElements {
+    id: number;
+    startday: string;
+    startmonth: string;
+    starttime: string;
+    endtime: string;
+    title: string;
+    place: string;
+}
 
 @Component({
   selector: 'app-cal-list',
@@ -11,20 +22,48 @@ import { AppEvents } from 'src/app/services/models/events';
 })
 export class CalListComponent implements OnInit {
   @Input() selectedDate: Date;
+  @Input() data: Observable<any>;
 
   appEvents: AppEvents;
-
+  stringAppEvents: Array<listEventElements>
   constructor(private _ruler: ViewportRuler, private _router: Router, private _calService: CalendarService) {
   }
 
   ngOnInit(): void {
-    this._calService.getAllEvents().subscribe(ev => {
-      this.appEvents = ev;
+    this.data.subscribe(rawData => {
+      this.appEvents = rawData;
     })
+
+    this.stringAppEvents = this._appEventsToString(this.appEvents)
   }
 
   showDetails(id: number): void {
     this._router.navigate(['calendar/event', id]);
+  }
+
+
+  private _appEventsToString(appEvents: AppEvents): Array<listEventElements> {
+    let array: listEventElements[]=[];
+    appEvents.forEach(ev => {
+      array.push(this._appEventToString(ev))
+    })
+
+    return array
+  }
+
+  private _appEventToString(appEvent: AppEvent): listEventElements {
+    let startdate = new Date(appEvent.start)
+    let enddate = new Date(appEvent.end)
+    let element = {
+      id: appEvent.id,
+      startday: startdate.getDate().toString(),
+      startmonth: startdate.getMonth().toString(),
+      starttime: startdate.getHours().toString() + ":" + startdate.getMinutes().toString(),
+      endtime: enddate.getHours().toString() + ":" + enddate.getMinutes().toString(),
+      title: appEvent.title,
+      place: appEvent.place
+    }
+    return element
   }
 
 }
--- src/app/calendar/calendar/calendar.component.ts
+++ src/app/calendar/calendar/calendar.component.ts
@@ -1,5 +1,9 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
+import { CalendarService } from 'src/app/services/calendar.service';
+import { AppEvents } from 'src/app/services/models/events';
 
 @Component({
   selector: 'app-calendar',
@@ -9,19 +13,22 @@ import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 export class CalendarComponent implements OnInit, OnDestroy {
   isSelected: Date
   openSidebar: boolean
+  eventList: Observable<AppEvents>
 
   mobileQuery: MediaQueryList;
 
   private _mobileQueryListener: () => void;
 
   constructor(private changeDetectorRef: ChangeDetectorRef,
-      private media: MediaMatcher) {
+      private media: MediaMatcher, private _calS: CalendarService) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addEventListener('change', this._mobileQueryListener);
   }
 
-  ngOnInit(): void {  }
+  ngOnInit(): void {
+    this.eventList = this._calS.getAllEvents();
+  }
 
   ngOnDestroy(): void {
     this.mobileQuery.removeEventListener('change', this._mobileQueryListener);
