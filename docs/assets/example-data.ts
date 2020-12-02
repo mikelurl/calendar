e8df7c35d868344b46b4027e59a674941a233ba4
mikelurban <mhop@outlook.com>
2020-12-02 21:41:40 +0100

    new: github.io build

--- docs/assets/example-data.ts
+++ docs/assets/example-data.ts
@@ -1,88 +1,13 @@
-import { AppEvents, AppEventsDetail } from 'src/app/services/models/events'
-
-
-export const ExampleEvents: AppEvents = [
-    {
-        id: 1,
-        title: "Example Title",
-        start: new Date(2019, 11, 20),
-        end: new Date(2019, 11, 20),
-        place: "Köln"
-    },
-    {
-        id: 2,
-        title: "Example Title",
-        start: new Date(Date.now()),
-        end: new Date(Date.now()),
-        place: "Potzdamer Platz 3, Berlin"
-    },
-    {
-        id: 3,
-        title: "Example Title",
-        start: new Date(2020, 12, 24),
-        end: new Date(20, 12, 26),
-        place: "München"
-    },
-    {
-        id: 4,
-        title: "Example Title",
-        start: new Date(2021, 11, 20),
-        end: new Date(2021, 11, 20),
-        place: "Köln"
-    },
-    {
-        id: 5,
-        title: "Example Title",
-        start: new Date(2021, 11, 19),
-        end: new Date(2021, 11, 19),
-        place: ""
-    },
-]
+import { AppEventsDetail } from 'src/app/services/models/events'
 
 export const ExampleEventsDetail: AppEventsDetail = [
     {
         id: 1,
-        title: "Example Title",
-        start: new Date(2019, 11, 20),
-        end: new Date(2019, 11, 20),
-        place: "Köln",
-        description: "",
-        participants: []
-    },
-    {
-        id: 2,
-        title: "Example Title",
+        title: "Example Event (Read description)",
         start: new Date(Date.now()),
         end: new Date(Date.now()),
-        place: "Potzdamer Platz 3, Berlin",
-        description: "",
-        participants: [""]
-    },
-    {
-        id: 3,
-        title: "Example Title",
-        start: new Date(2020, 12, 24),
-        end: new Date(20, 12, 26),
-        place: "München",
-        description: "",
-        participants: [""]
-    },
-    {
-        id: 4,
-        title: "Example Title",
-        start: new Date(2021, 11, 20),
-        end: new Date(2021, 11, 20),
-        place: "Köln",
-        description: "",
-        participants: [""]
-    },
-    {
-        id: 5,
-        title: "Example Title",
-        start: new Date(2021, 11, 19),
-        end: new Date(2021, 11, 19),
-        place: "",
-        description: "",
-        participants: [""]
-    },
+        place: "Europe",
+        description: "All events unencrypted saved in your local browser storage. This is just a test app!",
+        participants: []
+    }
 ]
